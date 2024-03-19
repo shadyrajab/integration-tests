f48a405a23b5369a8af579363af98da2b710a821
shady <shadyrajaab@gmail.com>
2024-03-19 14:20:47 -0300

    feat: finalizar test_vendas

--- test_vendas_integration.py
+++ test_vendas_integration.py
@@ -13,4 +13,51 @@ def test_vendas_endpoint():
         assert response.status_code == 200
         pass
 
+    def post_vendas():
+        response = requests.post(
+            url,
+            json={
+                "cnpj": "47270276000153",
+                "telefone": "61996076351",
+                "consultor": "PEDRO HENRIQUE",
+                "data": "19-03-2024",
+                "gestor": "INTEGRATION TEST",
+                "preco": 13.99,
+                "plano": "SMART EMPRESAS 6 GB",
+                "volume": 5,
+                "equipe": "FREECEL",
+                "tipo": "AVANÇADA",
+                "ja_cliente": False,
+                "email": "shadyrajaab@gmail.com",
+                "ddd": "61",
+                "status": "CONCLUÍDO",
+                "numero_pedido": "AHSHDJAHSHDJS",
+            },
+            headers=HEADERS,
+        )
+
+        assert response.status_code == 200, response.text
+        return response.json()["id"]
+
+    def update_vendas(id):
+        response = requests.put(
+            url,
+            json={"id": id, "status": "CONCLUÍDO / PAGO", "tipo": "MIGRAÇÃO"},
+            headers=HEADERS,
+        )
+
+        assert response.status_code == 200, response.text
+        pass
+
+    def delete_vendas(id):
+        response = requests.put(url, json={"id": id}, headers=HEADERS)
+
+        assert response.status_code == 200, response.text
+        pass
+
+
+
     get_vendas()
+    id = post_vendas()
+    update_vendas(id)
+    delete_vendas(id)
